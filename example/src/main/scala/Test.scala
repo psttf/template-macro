73444eee3ae8432510c507efbf4b2c495a2b84f9
Grigory Pomadchin <gr.pomadchin@gmail.com>
2015-11-19 15:41:26 +0300

    +ordering test

--- example/src/main/scala/Test.scala
+++ example/src/main/scala/Test.scala
@@ -3,15 +3,15 @@ import reflect.runtime.universe.TypeTag
 
 case class SW(s: String)
 case class IW(c: Int)
-case class BW(d: Double)
-case class PD(sw: SW, iw: IW, bw: BW)
+case class DW(d: Double)
+case class PD(sw: SW, iw: IW, bw: DW)
 
-@template((i: Int, s: String, d: Double) => PD(SW(s), IW(i), BW(d)))
+@template((i: Int, s: String, d: Double) => PD(SW(s), IW(i), DW(d)))
 object Test extends App {
   def typeToString[T](o: T)(implicit t: TypeTag[T]) = t.tpe.toString
 
   lazy val appl   = apply(2, "sss", 2d)
-  lazy val unappl = unapply(PD(SW("s"), IW(1100), BW(28d)))
+  lazy val unappl = unapply(PD(SW("s"), IW(1100), DW(28d)))
 
   println(s"apply: ${appl}: ${typeToString(appl)}")
   println(s"unapply: ${unappl}: ${typeToString(unappl)}")
